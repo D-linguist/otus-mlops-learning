12160e6d2f74ded235caa302bf05b47424b7dee0
Andrei <alexander.jones333@yahoo.com>
2024-02-17 00:50:16 +0300

    k8s

--- hw8/app/test_sample.py
+++ hw8/app/test_sample.py
@@ -14,22 +14,22 @@ def test_predict():
     with TestClient(app) as client:
         transaction = {
             "transaction_id": 126321480,
-            "ts": "2019-11-10 00:02:19",
-            "tx_amount": 32.7,
+            "ts": "2022-01-10 00:02:00",
+            "tx_amount": 22.4,
             "is_weekend": 1,
             "is_night": 1,
             "customer_id_nb_tx_1": 4,
-            "customer_id_avg_amount_1": 40.3425,
-            "customer_id_nb_tx_7": 18,
-            "customer_id_avg_amount_7": 50.448889,
-            "customer_id_nb_tx_30": 69,
-            "customer_id_avg_amount_30": 55.98971,
+            "customer_id_avg_amount_1": 30.325,
+            "customer_id_nb_tx_7": 20,
+            "customer_id_avg_amount_7": 60.2436,
+            "customer_id_nb_tx_30": 70,
+            "customer_id_avg_amount_30": 75.7467,
             "terminal_id_nb_tx_1": 1624,
             "terminal_id_risk_1": 1.0,
             "terminal_id_nb_tx_7": 10948,
             "terminal_id_risk_7": 1.0,
             "terminal_id_nb_tx_30": 46816,
-            "terminal_id_risk_30": 0.399949,
+            "terminal_id_risk_30": 0.23599,
             "tx_fraud": 1
         }
         response = client.post("/predict/", json=transaction)
